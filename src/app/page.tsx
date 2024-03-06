0d115c393d86355d352f9b8f01d5e42aaea018e8
Martin Macharia <mosmartin5@gmail.com>
2024-03-06 15:06:13 +0100

    feat: add an accent colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
       </h1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
         Browse more than{" "}
-        <span className="font-bold text-[#a4f839] italic">10,000</span> events
+        <span className="font-bold text-accent italic">10,000</span> events
         around you.
       </p>
 
       <form className="w-full sm:w-[580px]" action="">
         <input
-          className="w-full h-16 rounded-lg bg-white/5 px-6 outline-none ring-[#a4f839]/50 focus:ring-2 focus:bg-white/10 transition"
+          className="w-full h-16 rounded-lg bg-white/5 px-6 outline-none ring-accent/50 focus:ring-2 focus:bg-white/10 transition"
           type="text"
           placeholder="Search events in any city..."
           spellCheck="false"
