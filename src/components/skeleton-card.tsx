a35b88d8f4d9c8e4fb4a4b7eacc0cff4c4f4b17e
Martin Macharia <mosmartin5@gmail.com>
2024-03-19 15:14:10 +0100

    update the loading state

---
+++ src/components/skeleton-card.tsx
@@ -0,0 +1,15 @@
+import Skeleton from "./skeleton";
+
+type SkeletonCardProps = {
+  className?: string;
+};
+
+export default function SkeletonCard({ className }: SkeletonCardProps) {
+  return (
+    <div className="space-y-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <Skeleton className="h-4 w-[250px]" />
+      <Skeleton className="h-4 w-[200px]" />
+    </div>
+  );
+}
